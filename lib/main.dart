2e4073fde11ee337275d93df2d02e1239b7816a1
Arvind Chauhan <psych3080@gmail.com>
2022-03-11 12:52:47 +0530

    chapter 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,25 +14,46 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Awesome App'),
-      ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            // shape: BoxShape.circle,
-            boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5)],
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.teal,
-            gradient: LinearGradient(colors: [Colors.yellow, Colors.pink]),
-          ),
-          width: 100,
-          height: 100,
-          child: Text('I am a box'),
+        appBar: AppBar(
+          title: Text('Awesome App'),
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: Container(
+              color: Colors.black,
+              width: 200,
+              height: 400,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      width: 100,
+                      height: 100,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      width: 100,
+                      height: 100,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      width: 100,
+                      height: 100,
+                      color: Colors.yellow,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
